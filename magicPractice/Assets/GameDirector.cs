3dc10c6fadab8c21cdea7b61d0178ffc16e3bb73
jeongbeen <binibin0122@gmail.com>
2018-05-31 14:35:07 +0900

    a
    aaa

---
+++ magicPractice/Assets/GameDirector.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+    string nameTemplate = @"{0}";
+    List<GameObject> cubes = new List<GameObject>();
+    GameObject cube;
+    GameObject cubeNumber;
+
+    int count;
+    float positionX, positionZ;
+    List<GameObject> magicCube = new List<GameObject>();
+    GameObject[] magic = new GameObject[9];
+
+    void Start () {
+        count = 0;
+        for (int i = 1; i < 10; i++)
+        {
+            string cubeName = string.Format(nameTemplate, i);
+            cube = GameObject.Find(cubeName);
+            cubes.Add(cube);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddMagicCube();
+    }
+
+    void AddMagicCube()
+    {
+        foreach (GameObject cube in cubes)
+        {
+            positionX = cube.transform.position.x;
+            positionZ = cube.transform.position.z;
+
+            if (positionX < -1.5f && positionX > -2.5f)
+            {
+                if (positionZ > 1.5f && positionZ<2.5f) { magic[0] = cube;  }
+                else if (positionZ < -1.5f && positionZ > -2.5f) { magic[6] = cube; }
+                else if (positionZ<1.5f && positionZ >- 1.5f) { magic[3] = cube; }
+            }
+            else if (positionX > 1.5f && positionX<2.5f)
+            {
+                if (positionZ > 1.5f && positionZ < 2.5f) { magic[2] = cube;}
+                else if (positionZ < -1.5f && positionZ > -2.5f) { magic[8] = cube;}
+                else if (positionZ < 1.5f && positionZ > -1.5f) { magic[5] = cube;}
+            }
+            else if (positionX < 1.5f && positionX > -1.5f)
+            {
+                if (positionZ > 1.5f && positionZ < 2.5f) { magic[1] = cube; }
+                else if (positionZ < -1.5f && positionZ > -2.5f) { magic[7] = cube;}
+                else if (positionZ < 1.5f && positionZ > -1.5f) { magic[4] = cube;}
+            }
+        }
+    }
+
+    bool checkMagic()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Debug.Log(magic[i].name);
+        }
+        if ((Convert.ToInt32(magic[0].name) + Convert.ToInt32(magic[4].name) + Convert.ToInt32(magic[8].name) == 15) &&
+            (Convert.ToInt32(magic[2].name) + Convert.ToInt32(magic[4].name) + Convert.ToInt32(magic[6].name) == 15))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void GameFailed()
+    {
+        foreach (GameObject cube in cubes)
+        {
+            cube.transform.position = new Vector3(UnityEngine.Random.Range(-5, 5), 3, UnityEngine.Random.Range(-7, -4));
+        }
+    }
+    public void Submit()
+    {
+        if (checkMagic()) { Debug.Log("성공했어요!"); }
+        else { Debug.Log("틀렸어!"); GameFailed(); }
+    }
+}
---
+++ magicPractice/Assets/MagicController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicController : MonoBehaviour {
+
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor= Color.red;
+    private bool dragging = false;
+    private float distance;
+    float positionX;
+    float positionZ;
+    GameObject cubeNumber;
+
+    public int magicNumber;
+
+    //void OnMouseEnter()
+    //{
+    //    renderer.material.color = mouseOverColor;
+    //}
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+    void OnMouseUp()
+    {
+        dragging = false;
+
+        //if (this.transform.position.x < -2) { positionX = -2.0f; }
+        //else if (this.transform.position.x > 2) { positionX = 2.0f; }
+        //else { positionX = 0.0f; }
+
+        //if (this.transform.position.z < -2) { positionZ = -2.0f; }
+        //else if (this.transform.position.z > 2) { positionZ = 2.0f; }
+        //else { positionZ = 0.0f; }
+
+        this.transform.position = new Vector3(this.transform.position.x, 0.0f, this.transform.position.z);
+    }
+	// Use this for initialization
+	void Start () {
+        //cubeNumber = transform.Find("Number").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+        this.transform.rotation= Quaternion.Euler(0,0,0);
+	}
+
+
+}
